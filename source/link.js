7fae99ef0cefc49809c9e721be168ca71482b83d
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-06-03 11:52:48 +0100

    advance on the beat

--- source/link.js
+++ source/link.js
@@ -1,3 +1,6 @@
+import { shared } from "./main.js"
+import { getPeak } from "./nogan/source/nogan.js"
+
 const BPM = 122
 
 let lastBeats = 0
@@ -14,5 +17,12 @@ export const frame = (time = 0) => {
 }
 
 const beat = () => {
-	console.log("BEAT")
+	const now = shared.nogan.root
+
+	for (const [id] of now.children) {
+		const afterChild = getPeak(now, {
+			id,
+			// ??? Todo: Figure out. getPeak requires very specific arguments. I guess I just do all of them? Or can I group stuff together? Maybe just do indivudally first!
+		})
+	}
 }
