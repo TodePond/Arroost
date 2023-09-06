81a404e8544cef2eb2913889ab0040f78e70181e
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-09-06 10:36:41 +0100

    decrease debug size

--- source/arroost/entities/cells/dummy-creation.js
+++ source/arroost/entities/cells/dummy-creation.js
@@ -81,7 +81,7 @@ export class DummyCreation extends Entity {
 			return fireCell(shared.nogan, { id: this.tunnel.id })
 		})
 
-		const count = e.button === 0 ? 1 : 100
+		const count = e.button === 0 ? 1 : 10
 
 		let n = 0
 		for (let i = 0; i < count; i++) {
