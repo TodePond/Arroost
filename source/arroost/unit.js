38165d0f336e50dd9fa8716751eec040aab8ca48
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-12-29 15:38:38 +0000

    zoomout

--- source/arroost/unit.js
+++ source/arroost/unit.js
@@ -8,6 +8,7 @@ export const TENTH = FULL / 10
 
 export const ZOOMING_IN_THRESHOLD = 5.0
 export const ZOOM_IN_THRESHOLD = 40.0
+export const ZOOMING_OUT_THRESHOLD = 0.3
 
 export const PARENT_SCALE = 100
 export const CHILD_SCALE = 1 / PARENT_SCALE
