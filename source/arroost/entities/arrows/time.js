0df980df33925192e9d8fad62fb7888b8a17c5aa
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-12-30 17:01:39 +0000

    zoom in wire cells

--- source/arroost/entities/arrows/time.js
+++ source/arroost/entities/arrows/time.js
@@ -69,10 +69,14 @@ export class ArrowOfTime extends Entity {
 		this.flaps = this.attach(new ArrowOfTiming({ wire: this.tunnel.id }))
 		this.colour = this.attach(new ArrowOfColour({ wire: this.tunnel.id }))
 
-		shared.scene.layer.timing.append(this.flaps.dom)
-		shared.scene.layer.timing.append(this.colour.dom)
 		this.dom.append(this.line.dom)
-		// this.dom.append(this.flaps.dom)
+		if (!this.isPreview) {
+			shared.scene.layer.timing.append(this.flaps.dom)
+			shared.scene.layer.timing.append(this.colour.dom)
+		} else {
+			this.dom.append(this.flaps.dom)
+			this.dom.append(this.colour.dom)
+		}
 
 		// Style elements
 		this.flaps.dom.style.fill.set(WHITE.toString())
